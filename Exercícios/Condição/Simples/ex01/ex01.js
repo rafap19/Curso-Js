5e5a10284d7245ee00826f393bfa2bb67cdedf23
Rafael Sousa <rafap.19@hotmail.com>
2021-12-31 07:44:52 -0300

    comit

---
+++ Exercícios/Condição/Simples/ex01/ex01.js
@@ -0,0 +1,14 @@
+let corda = 5
+    console.log('Um violão é composto por um total de 6 cordas')
+        if(corda < 6){
+            console.log('Esta faltando uma corda!')
+        }
+
+        let moto = 30.3
+            console.log('Sistema de limite de velocidade')
+                if(moto >30){
+                    console.log('VOCÊ FOI MULTADO')
+                }
+            console.log('Dirija com cuidado')
+
+        //Condições Simples
---
+++ Exercícios/Condição/Simples/ex02/ex02.js
@@ -0,0 +1,6 @@
+let moto = 30.3
+console.log('Sistema de limite de velocidade')
+    if(moto >30){
+        console.log('VOCÊ FOI MULTADO')
+    }
+console.log('Dirija com cuidado')
