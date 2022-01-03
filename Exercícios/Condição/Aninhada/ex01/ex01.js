7894b007c51cd49999d924be017159d2f257c0df
Rafael Sousa <rafap.19@hotmail.com>
2022-01-03 14:49:42 -0300

    comit

---
+++ Exercícios/Condição/Aninhada/ex01/ex01.js
@@ -0,0 +1,17 @@
+let idade = 18
+console.log('SISTEMA DE IDENTIDADE')
+    if(idade < 16){
+        console.log('Não vota')
+    }else if(idade == 16 || idade == 17){
+        console.log('Voto não obrigatório')
+    }else if(idade >= 18 && idade < 65){
+        console.log('Voto Obrigatório')
+    }else if(idade >=65 && idade <= 75){
+        console.log('Voto não obrigatório')
+    }else{
+        console.log('Inapto para votar')
+    }
+     
+    
+        
+
