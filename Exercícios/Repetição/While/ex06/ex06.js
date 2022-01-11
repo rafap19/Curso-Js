60bb3f580a615d475af1ba1bbaf66fba9b408dbf
Rafael Sousa <rafap.19@hotmail.com>
2022-01-11 17:56:26 -0300

    comit

---
+++ Exercícios/Repetição/While/ex06/ex06.js
@@ -0,0 +1,5 @@
+let hora = 3
+    while(hora < 8){
+        console.log('Continue dormindo')
+            hora++
+    }
---
+++ Exercícios/Repetição/While/ex07/ex07.js
@@ -0,0 +1,5 @@
+let velocidade_moto = 30
+    while(velocidade_moto <= 30){
+        console.log('a velocidade está muito baixa,acelere')
+        velocidade_moto = velocidade_moto + 1
+    }
