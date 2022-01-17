c22b6ff95e4b51aec59ed810fe1974e892d5f31e
Rafael Sousa <rafap.19@hotmail.com>
2022-01-17 16:03:22 -0300

    comit

--- Exercícios/Condição/Aninhada/ex11/Site_exemplo/ex11.js
+++ Exercícios/Condição/Aninhada/ex11/Site_exemplo/ex11.js
@@ -26,20 +26,26 @@ let btmaior = document.querySelector('input#btmaior');
         let btmenor = document.querySelector('input#btmenor');
             btmenor.addEventListener('click',Menor);
 
-            function Menor(){
-                var n1 = parseFloat(document.getElementById("n1").value);
-                var n2 = parseFloat(document.getElementById("n2").value);
-                var n3 = parseFloat(document.getElementById("n3").value);
-         
-                var menor = num1;
-         
-                if(n2 < menor)
-                 menor = n2;
-                if(n3 < menor)
-                 menor = n3;
-         
-                alert("Menor: "+menor);
-               }
+                function Menor(){
+                    let n1 = Number(document.querySelector('input#n1').value); 
+
+                    let n2 = Number(document.querySelector('input#n2').value); 
+
+                    let n3 = Number(document.querySelector('input#n3').value); 
+
+                    let menor = n1;
+
+                        if(n2 < menor && n2 < n3)
+                            menor = n2;
+
+                        else if(n3 < menor && n3 < n2)
+                            menor = n3;
+
+                        else
+                            menor = n1;
+
+                            alert(`O valor menor é igual á: ${menor}`);
+                }
                 
                     
                 
