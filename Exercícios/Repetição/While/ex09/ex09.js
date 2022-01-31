b8dcd2cce801e8326472862b4fdbc1d4271baaba
Rafael Sousa <rafap.19@hotmail.com>
2022-01-31 17:30:49 -0300

    comit

---
+++ Exercícios/Repetição/While/ex09/ex09.js
@@ -0,0 +1,11 @@
+function Resultado(){
+    let numero = Number(document.querySelector('input#numero').value);
+        while(numero.value <= 10){
+            alert(` O valor inserído foi ${numero}`);
+        }
+            alert(` Número Inválido`);
+                numero++
+        
+     
+
+}
