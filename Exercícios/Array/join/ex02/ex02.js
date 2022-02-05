1cf0444c880b96314f338c1e859050c16856c545
Rafael Sousa <rafap.19@hotmail.com>
2022-02-04 23:49:06 -0300

    comit

--- Exercícios/Array/join/ex02/ex02.js
+++ Exercícios/Array/join/ex02/ex02.js
@@ -27,8 +27,26 @@ var usuario =[
         var concat = array1.concat(array2);
             console.log(concat);
 
-        //Podemos fazer de uma maneira mais inteligente criando um array de arrays, para isso basta inserir as variáveis com os arrays inseridos antecedidas de ...  que significa spread ou espalhar')
+        //Podemos fazer de uma maneira mais inteligente criando um array de arrays, para isso basta inserir as variáveis com os arrays inseridos antecedidas de ...  que significa spread ou espalhar')//
 
             console.log([...array1, ...array2]);
+        
 
-    
+        /*Array destructuring
+            é um conceito aonde imprimimos os valores dos arrays de maneira individual através de variáveis,podemos junta-las através de vírgulas.
+                               */
+            console.log(usuario);
+                var nome = usuario[0];
+                var idade = usuario[1];
+                var materia = usuario[2];
+                var ano = usuario[3];
+                var nacionalidade = usuario[4];
+                var estado = usuario[5];
+                    console.log(nome,idade,materia,ano,nacionalidade,estado);
+
+                      /*Podemos fazer de uma maneira mais simplificada o mesmo código da forma abaixo,onde declaramos um variavel,escolhemos os nome das nossas variaveis e igualamos a variavel aonde estes arrays estão contidos, com isso ele automáticamente já filtra as posições dos arrays,com isso nós podemos imprimir os valores de cada array sem se preocupar aonde ele está.
+                               */
+
+                var [nome,idade,materia,ano,nacionalidade,estado]= usuario;
+                    console.log(nome,idade,ano);
+                
