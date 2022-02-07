8becacca9c2de06d6bb832cc13a53a7563813b03
Rafael Sousa <rafap.19@hotmail.com>
2022-02-07 14:43:24 -0300

    comit

--- Exercícios/Functions/ex02/ex02.js
+++ Exercícios/Functions/ex02/ex02.js
@@ -5,5 +5,6 @@ function escrevaMensagem(mensagem) {
 }
 
 escrevaMensagem('Hello World');
+escrevaMensagem('Deus é bom')
 
-/* Neste caso dando um parâmetro mensagem para a função,determinamos que console.log irá executar esse parâmetro, isso faz com que por exemplo quando chamarmos a função vamos poder escrever oque quiser,visto que o parâmetro mensagem é determinado pelo console.log,ou seja ao invés de aparecer uma frase escrita ele nos da autonomia de escrever oque quiser */
+/* Neste caso dando um parâmetro mensagem para a função,determinamos que console.log irá executar esse parâmetro, isso faz com que por exemplo quando chamarmos a função vamos poder escrever oque quiser,visto que o parâmetro mensagem é determinado pelo console.log,ou seja ao invés de aparecer uma frase escrita ele nos da autonomia de escrever oque quiser,temos então uma função customizável. */
