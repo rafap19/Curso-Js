565bdfcb2a4fddb5d083ca19b1d7860bf90b03f0
Rafael Sousa <rafap.19@hotmail.com>
2022-02-07 14:41:57 -0300

    comit

---
+++ Exercícios/Functions/ex01/ex01.js
@@ -0,0 +1,9 @@
+/* Uma parte de código que podemos reutilizar,damos um nome para essa função e podemos chamá-la caso precisarmos,para criarmos uma função basta escrever function + o nome que você quer para a função + () */
+
+function escreva(){
+    console.log('Olá mundo')
+}
+
+escreva();
+
+/* Basta escrever o nome da função adionando () ao lado para chamá-la*/
