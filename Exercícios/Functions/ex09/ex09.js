867d3cdf1c505be8f5af579837e28c48fcedf711
Rafael Sousa <rafap.19@hotmail.com>
2022-02-07 18:53:40 -0300

    comit

---
+++ Exercícios/Functions/ex09/ex09.js
@@ -0,0 +1,8 @@
+/* Funções default são aquelas que quando não passados os parâmetros ela mostra parâmetros default*/
+
+function escreveNome(nome = 'Rafael', idade = '21') {
+    console.log(nome);
+    console.log(idade);
+}
+    escreveNome();
+    
