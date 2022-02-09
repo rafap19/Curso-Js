c6e092140660b2021f2b0a319151b62e558fbcca
Rafael Sousa <rafap.19@hotmail.com>
2022-02-09 13:32:00 -0300

    comit

---
+++ Exercícios/Escopos/ex04/ex04.js
@@ -0,0 +1,6 @@
+/* escopo lexical: é aquele que o js ja sabe o valor da variável antes de executar porque o valor já é definido */
+
+function usuario() {
+    var nome = 'Ayrton'
+        console.log(nome)
+}
