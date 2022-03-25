26e4aa0ff037101af1543a6b430eec707c90e27d
Rafael Sousa <rafap.19@hotmail.com>
2022-03-25 16:55:06 -0300

    comit

---
+++ Curso_Dev_Aprender/miniProjetos/projeto1/trocando_valores.js
@@ -0,0 +1,19 @@
+/*troque os valores imprimidos, ao invés de vermelho e azul tem que aparecer azul e vermelho */
+
+let a = 'vermelho';
+let b = 'azul';
+let ba = [a,b];
+
+console.log(ba[1]);
+console.log(ba[0]);
+
+/*utilizei array para resolver */
+
+
+
+
+
+
+
+
+
