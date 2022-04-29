80bfddf49b7dc9f33e2cf57c89d6f29ca45229b5
Rafael Sousa <rafap.19@hotmail.com>
2022-04-29 17:10:58 -0300

    comit-loop-exercises

---
+++ Curso_Dev_Aprender/exercicios/laços/for/for/ex07.js
@@ -0,0 +1,25 @@
+/*Crie uma função que exiba apenas os valores pares de um array */
+
+
+let arr = [1,2,3,4,5,6,7,8,9]
+
+function showEvenNum(){
+
+    for(let i = 0; i <= 9; i++){
+
+        if(i % 2 === 0) {
+            console.log(i)
+        }
+    }
+
+}
+
+showEvenNum()
+
+
+    
+
+
+
+
+
---
+++ Curso_Dev_Aprender/exercicios/laços/for/for/ex08.js
@@ -0,0 +1,15 @@
+/*Utilizando a lista do exercício anterior, crie um algoritmo que mostre de maneira decrescente os elementos desta lista. */
+
+
+let arr = [1,2,3,4,5,6,7,8,9]
+
+
+function showListOnDescendingOrder(){
+
+    for(let i = 9;i >= 1; i--){
+
+        console.log(i)
+    }
+}
+
+ showListOnDescendingOrder()
